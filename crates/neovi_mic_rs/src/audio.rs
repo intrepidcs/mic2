1617aa93cc6c0b2f36bdbc806743ddf7c4ecda69
David Rebbe <drebbe@intrepidcs.com>
2023-07-27 23:56:16 -0400

    WIP audio

--- crates/neovi_mic_rs/src/audio.rs
+++ crates/neovi_mic_rs/src/audio.rs
@@ -1,6 +1,77 @@
+use crate::types::{Error, Result};
 use core::time;
+use std::cell::RefCell;
 
-use sfml;
+use regex::Regex;
+use sfml::{self, audio::SoundBufferRecorder};
+
+#[derive(Debug)]
+pub struct Audio {
+    /// Typically is "Monitor of PCM2912A Audio Codec Analog Stereo"
+    pub capture_name: String,
+    /// Index of the capture device if multiple, starts at 1
+    /// "Monitor of PCM2912A Audio Codec Analog Stereo #2" would be an index of 2
+    pub index: u32,
+    recorder: SoundBufferRecorder,
+}
+
+impl Audio {
+    pub fn find_neovi_mic2_audio() -> Result<RefCell<Vec<Self>>> {
+        // "Monitor of PCM2912A Audio Codec Analog Stereo"
+        // "Monitor of PCM2912A Audio Codec Analog Stereo #2"
+        let re = Regex::new(r"Monitor of PCM2912A Audio Codec").unwrap();
+        let re_index = Regex::new(r"\d+$").unwrap();
+        let capture_devices = RefCell::new(Vec::new());
+
+        if !sfml::audio::capture::is_available() {
+            return Err(Error::CriticalError("Audio capture is unavailable!".into()));
+        }
+        let devices = sfml::audio::capture::available_devices();
+        for device in &*devices {
+            // Match our expected audio device
+            if !re.is_match(device.to_str().unwrap()) {
+                continue;
+            }
+            // Find the index of the capture device
+            let index = match re_index.find(device.to_str().unwrap()) {
+                Some(m) => m.as_str().parse::<u32>().unwrap(),
+                None => 1,
+            };
+            // Create the recorder
+            let mut recorder = SoundBufferRecorder::new();
+            recorder.set_device(device.to_str().unwrap()).unwrap();
+            // Create the Audio device
+            capture_devices.borrow_mut().push(Self {
+                capture_name: device.to_string(),
+                index,
+                recorder,
+            });
+        }
+        Ok(capture_devices)
+    }
+
+    pub fn start(&mut self, sample_rate: u32) -> Result<()> {
+        if !self.recorder.start(sample_rate) {
+            return Err(Error::CriticalError("Failed to start recording!".into()))
+        }
+        Ok(())
+    }
+
+    pub fn stop(&mut self) -> Result<()> {
+        self.recorder.stop();
+        Ok(())
+    }
+
+    pub fn save_to_file(&mut self, fname: impl Into<String>) -> Result<()> {
+        let fname: String = fname.into();
+        if !self.recorder.buffer().save_to_file(fname.as_str()) {
+            return Err(Error::CriticalError(
+                format!("Failed to save capture from {} to file {}", self.capture_name, fname).into())
+            );
+        }
+        Ok(())
+    }
+}
 
 pub fn record_default_capture() {
     if !sfml::audio::capture::is_available() {
@@ -15,7 +86,7 @@ pub fn record_default_capture() {
     std::thread::sleep(time::Duration::from_millis(3000));
     recorder.stop();
     println!("Finished Recording");
-    
+
     let buffer = recorder.buffer();
     if !buffer.save_to_file("/home/drebbe/test.wav") {
         panic!("Failed to save file!");
@@ -26,6 +97,15 @@ pub fn record_default_capture() {
 mod test {
     use super::*;
 
+    #[test]
+    fn test_find_neovi_mic2_capture() -> Result<()> {
+        let devices = Audio::find_neovi_mic2_audio()?;
+        println!("{devices:#?}");
+        for device in devices.borrow_mut() {
+            *device.start(44_100);
+        }
+        Ok(())
+    }
     #[test]
     fn test_record_default_capture() {
         record_default_capture();
