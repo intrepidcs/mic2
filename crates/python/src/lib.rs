1a8150925c29e03fe18ed8558931bde7b0b0127d
David Rebbe <drebbe@intrepidcs.com>
2023-10-17 00:55:56 -0400

    Started python module

---
+++ crates/python/src/lib.rs
@@ -0,0 +1,23 @@
+use pyo3::prelude::*;
+
+use neovi_mic_rs::mic;
+pub mod types;
+
+#[pyfunction]
+fn find() -> PyResult<Vec<types::NeoVIMIC>> {
+    let devices = mic::find_neovi_mics()
+        .unwrap()
+        .into_iter()
+        .map(|x| types::NeoVIMIC::from(x))
+        .collect::<Vec<types::NeoVIMIC>>();
+    Ok(devices)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn neovi_mic(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<types::NeoVIMIC>()?;
+    m.add_class::<types::UsbDeviceInfo>()?;
+    m.add_function(wrap_pyfunction!(find, m)?)?;
+    Ok(())
+}
---
+++ crates/python/src/types.rs
@@ -0,0 +1,132 @@
+use std::sync::{Arc, Mutex};
+
+use pyo3::prelude::*;
+use neovi_mic_rs::mic;
+
+macro_rules! define_basic_py_object {
+    ($name:ident, $inner_name:ty) => {
+        #[pyclass]
+        #[derive(Debug)]
+        #[repr(transparent)]
+        pub struct $name(pub Arc<Mutex<$inner_name>>);
+
+        // Arc is only Send if T is Send so lets mark it as safe here
+        unsafe impl Send for $name {}
+
+        #[pymethods]
+        impl $name {
+            #[new]
+            fn py_new() -> Self {
+                Self::new()
+            }
+        }
+    };
+}
+
+macro_rules! define_basic_py_object_no_new {
+    ($name:ident, $inner_name:ty) => {
+        #[pyclass]
+        #[derive(Debug, Default, Clone)]
+        #[repr(transparent)]
+        pub struct $name(pub Arc<Mutex<$inner_name>>);
+
+        // Arc is only Send if T is Send so lets mark it as safe here
+        unsafe impl Send for $name {}
+    };
+}
+
+define_basic_py_object_no_new!(NeoVIMIC, mic::NeoVIMIC);
+#[pymethods]
+impl NeoVIMIC {
+    #[new]
+    fn py_new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+
+    fn __str__(&self) -> String {
+        let serial = self.0.lock().unwrap().get_serial_number();
+        format!("NeoVI MIC2 {serial}").to_string()
+    }
+
+    fn __repr__(&self) -> String {
+        let description = self.__str__();
+        format!("<NeoDevice {description}").to_string()
+    }
+
+    #[getter]
+    fn get_serial_number(&self) -> PyResult<String> {
+        Ok(self.0.lock().unwrap().get_serial_number())
+    }
+
+    #[getter]
+    fn has_gps(&self) -> PyResult<bool> {
+        Ok(self.0.lock().unwrap().has_gps())
+    }
+}
+
+impl NeoVIMIC {
+    fn new() -> Self {
+        Self {
+            0: Arc::new(Mutex::new(mic::NeoVIMIC { ..Default::default() })),
+        }
+    }
+
+    pub fn from(neovi_mic: mic::NeoVIMIC) -> Self {
+        Self {
+            0: Arc::new(Mutex::new(neovi_mic)),
+        }
+    }
+}
+
+define_basic_py_object_no_new!(UsbDeviceInfo, mic::UsbDeviceInfo);
+#[pymethods]
+impl UsbDeviceInfo {
+    #[new]
+    fn py_new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+
+    fn __str__(&self) -> String {
+        let serial = match &self.0.lock().unwrap().serial_number {
+            Some(s) => s.clone(),
+            None => "None".to_string(),
+        };
+        format!("NeoVI MIC2 {serial}").to_string()
+    }
+
+    fn __repr__(&self) -> String {
+        let description = self.__str__();
+        format!("<NeoDevice {description}").to_string()
+    }
+
+    #[getter]
+    fn vendor_id(&self) -> PyResult<u16> {
+        Ok(self.0.lock().unwrap().vendor_id)
+    }
+
+    #[getter]
+    fn product_id(&self) -> PyResult<u16> {
+        Ok(self.0.lock().unwrap().product_id)
+    }
+
+    #[getter]
+    fn bus_number(&self) -> PyResult<u8> {
+        Ok(self.0.lock().unwrap().bus_number)
+    }
+
+    #[getter]
+    fn address(&self) -> PyResult<u8> {
+        Ok(self.0.lock().unwrap().address)
+    }
+
+    /* TODO
+    #[getter]
+    fn device_type(&self) -> PyResult<u32> {
+        Ok(self.0.lock().unwrap().device_type.into())
+    }
+    */
+}
