334124c0e7bcc4b59bc7842e2f51421636620b65
David Rebbe <drebbe@intrepidcs.com>
2024-05-28 19:56:04 -0400

    added sfml and ftdi build scripts

---
+++ util/sfml.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+PARENT_DIR=$(pwd)
+SFML_VERSION="2.5.1"
+SFML_URL="https://github.com/SFML/SFML/archive/refs/tags/${SFML_VERSION}.tar.gz"
+
+mkdir -pv build_sfml && cd build_sfml
+echo "Downloading SFML source ${SFML_VERSION}..."
+curl -OL $SFML_URL
+tar -xf ${SFML_VERSION}.tar.gz
+
+echo "Building SFML ${SFML_VERSION}..."
+cd SFML-${SFML_VERSION}
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -Wno-dev
+cmake --build build
+
+echo "Setting up rust-sfml environment variables..."
+export SFML_LIB_DIR="$(pwd)/include"
+export SFML_INCLUDE_DIR="$(pwd)/build/lib"
+
+cd $PARENT_DIR
+
+echo "Done building SFML ${SFML_VERSION}!"
