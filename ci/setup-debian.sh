2dd8e3161562b25ce284271b034bb292722dd141
Trevor Gross <tgross@intrepidcs.com>
2024-05-30 02:56:27 -0500

    fix

--- ci/setup-debian.sh
+++ ci/setup-debian.sh
@@ -8,6 +8,12 @@ if [ -n "${CROSS_DEB_ARCH:-}" ]; then
   if [ "$CROSS_DEB_ARCH" = "musl-linux-armhf" ]; then
     # Cross gives the wrong debian architecutre
     arch="armhf"
+
+    if [ -n "$GITHUB_ENV" ]; then
+      # SFML can't find these for some reason
+      echo "SFML_INCLUDE_DIR=/usr/include/SFML" >> "$GITHUB_ENV"
+      echo "SFML_LIBS_DIR=/usr/lib/arm-linux-gnueabihf" >> "$GITHUB_ENV"
+    fi
   else
     arch="$CROSS_DEB_ARCH"
   fi
